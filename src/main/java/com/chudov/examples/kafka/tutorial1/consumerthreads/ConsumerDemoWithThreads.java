d97c8c4426ee7136a4511f22424f5ab4f0daa558
Oleksandr Chudov <oleksandr_chudov@epam.com>
2019-08-07 20:10:10 +0300

    Add consumer with Thread

---
+++ src/main/java/com/chudov/examples/kafka/tutorial1/consumerthreads/ConsumerDemoWithThreads.java
@@ -0,0 +1,45 @@
+package com.chudov.examples.kafka.tutorial1.consumerthreads;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.CountDownLatch;
+
+public class ConsumerDemoWithThreads {
+
+    private static final Logger logger = LoggerFactory.getLogger(ConsumerDemoWithThreads.class);
+
+    private static final String FIRST_TOPIC_NAME = "first_topic";
+    private static final String DEFAULT_BOOTSTRAP_SERVER = "127.0.0.1:9092";
+    private static final String GROUP_ID = "group_second";
+    public static final String MESSAGE_APP_IS_INTERRUPTED = "App is interrupted";
+
+    public static void main(String[] args) {
+
+        CountDownLatch latch = new CountDownLatch(1);
+        Runnable consumerRunnable = new ConsumerRunnable(FIRST_TOPIC_NAME, DEFAULT_BOOTSTRAP_SERVER, GROUP_ID, latch);
+        Thread consumerThread = new Thread(consumerRunnable);
+        consumerThread.start();
+
+        // add shutdown hook
+        Runtime.getRuntime().addShutdownHook(new Thread(
+                () -> {
+                    logger.info("Shutdown hook");
+                    ((ConsumerRunnable) consumerRunnable).shutdown();
+                    try {
+                        latch.await();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    logger.info("App has exited");
+                }
+        ));
+
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            logger.error(MESSAGE_APP_IS_INTERRUPTED, e);
+        }
+
+    }
+}
---
+++ src/main/java/com/chudov/examples/kafka/tutorial1/consumerthreads/ConsumerRunnable.java
@@ -0,0 +1,71 @@
+package com.chudov.examples.kafka.tutorial1.consumerthreads;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+
+public class ConsumerRunnable implements Runnable {
+
+    public static final String LOG_KEY = "Key: ";
+    public static final String LOG_VALUE = "Value: ";
+    public static final String LOG_PARTITION = "Partition: ";
+    public static final String LOG_OFFSET = "Offset: ";
+    Logger logger = LoggerFactory.getLogger(ConsumerRunnable.class);
+
+    private static final String MESSAGE_RECEIVED_SHUTDOWN_SIGNAL = "Received shutdown signal";
+    private static final String OFFSET_CONFIG = "earliest"; // earliest (from start of topic)/ latest (new messages) / none
+
+    private final CountDownLatch latch;
+    private final KafkaConsumer<String, String> consumer;
+
+    public ConsumerRunnable(String topic, String bootsrapServer, String groupId, CountDownLatch latch) {
+        consumer = new KafkaConsumer<>(getKafkaProperties(bootsrapServer, groupId));
+        consumer.subscribe(Collections.singleton(topic));
+        this.latch = latch;
+    }
+
+    private Properties getKafkaProperties(String bootsrapServer, String groupId) {
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootsrapServer);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, OFFSET_CONFIG);
+        return properties;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+                for (ConsumerRecord<String, String> record : records) {
+                    logger.info(LOG_KEY + record.key());
+                    logger.info(LOG_VALUE + record.value());
+                    logger.info(LOG_PARTITION + record.partition());
+                    logger.info(LOG_OFFSET + record.offset());
+                }
+            }
+        } catch (WakeupException e) {
+            logger.info(MESSAGE_RECEIVED_SHUTDOWN_SIGNAL);
+        } finally {
+            consumer.close();
+            latch.countDown();
+        }
+    }
+
+    public void shutdown() {
+        // to interrupt consumer.poll() by throw WakeupException
+        consumer.wakeup();
+    }
+}
